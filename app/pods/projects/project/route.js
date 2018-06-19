34026c854a33314d31f5a2b8a819eb3891945c7f
Stanislav Romanov <kaermorchen@gmail.com>
2018-06-19 21:43:16 +0300

    Added project template

--- app/pods/projects/project/route.js
+++ app/pods/projects/project/route.js
@@ -1,4 +1,9 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+  renderTemplate() {
+    this.render('projects.project', {
+      into: 'application',
+    });
+  }
 });
