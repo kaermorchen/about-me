2ddc56fac6e6e0f9abc65e4ee22657c088404139
Stanislav Romanov <kaermorchen@gmail.com>
2019-02-19 14:33:55 +0300

    Added adaptive styles

--- app/pods/index/route.js
+++ app/pods/index/route.js
@@ -1,18 +1,10 @@
 import { inject as service } from '@ember/service';
 import Route from '@ember/routing/route';
-import RSVP from 'rsvp';
 
 export default Route.extend({
   request: service(),
 
   model() {
-    return RSVP.hash({
-      model: this.request.fetch('/about.json').then(response => response.json()),
-      projects: this.store.findAll('project', { reload: true }).then(projects => projects.sortBy('createdAt').reverse())
-    });
-  },
-
-  setupController(controller, hash) {
-    controller.setProperties(hash);
+    return this.request.fetch('/about.json').then(response => response.json());
   }
 });
