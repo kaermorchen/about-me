b6f14554b10d9ce4388e653e411409d5545add19
Stanislav Romanov <kaermorchen@gmail.com>
2025-09-01 19:17:48 +0300

    Add filter

--- src/Project.tsx
+++ src/Project.tsx
@@ -1,4 +1,12 @@
-import { Drawer, Text, Portal, CloseButton, Stack } from "@chakra-ui/react";
+import {
+  Drawer,
+  Text,
+  Portal,
+  CloseButton,
+  Stack,
+  Heading,
+  Tag,
+} from "@chakra-ui/react";
 import { useNavigate, useParams } from "react-router";
 import projects from "./projects.json";
 import Carousel from "./Carousel";
@@ -23,7 +31,14 @@ export function Project() {
         <Drawer.Positioner>
           <Drawer.Content>
             <Drawer.Header>
-              <Drawer.Title>{selectedProject.attributes.name}</Drawer.Title>
+              <Drawer.Title>
+                <Stack direction="row" alignItems="baseline">
+                  <Heading>{selectedProject.attributes.name}</Heading>
+                  <Tag.Root>
+                    <Tag.Label>{selectedProject.attributes.type}</Tag.Label>
+                  </Tag.Root>
+                </Stack>
+              </Drawer.Title>
             </Drawer.Header>
             <Drawer.Body>
               <Stack gap={30}>
@@ -32,13 +47,27 @@ export function Project() {
                     (item) => `/about-me${item.src}`
                   )}
                 />
-                <Text>
-                  <div
-                    dangerouslySetInnerHTML={{
-                      __html: selectedProject.attributes.html,
-                    }}
-                  />
-                </Text>
+                <Stack gap={3}>
+                  <Text>
+                    <b>Year: </b>
+                    {new Date(
+                      selectedProject.attributes.createdAt
+                    ).getFullYear()}{" "}
+                    <b>Tags: </b>
+                    {selectedProject.attributes.tags.map((item) => (
+                      <Tag.Root key={item} marginRight={1}>
+                        <Tag.Label>{item} </Tag.Label>
+                      </Tag.Root>
+                    ))}
+                  </Text>
+                  <Text>
+                    <div
+                      dangerouslySetInnerHTML={{
+                        __html: selectedProject.attributes.html,
+                      }}
+                    />
+                  </Text>
+                </Stack>
               </Stack>
             </Drawer.Body>
             <Drawer.CloseTrigger asChild>
